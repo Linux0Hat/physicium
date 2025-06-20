de052eae1988d1277f6dc344a553242eab7e7c0b
Linux0Hat <adrien@d4x.fr>
2025-06-20 11:33:58 +0200

    fix workfolw again

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
+        filename: 'bundle.js', // changed from index.js to avoid conflicts
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -17,7 +17,7 @@ module.exports = {
             crateDirectory: path.resolve(__dirname, ".")
         }),
     ],
-    mode: 'development',
+    mode: process.env.NODE_ENV || 'development',
     experiments: {
         asyncWebAssembly: true
     },
